89a19152029bd260e29e7bc0317fe82bfa6de8d0
pletosu.lilian <lilianzau@mail.ru>
2020-11-04 16:35:46 +0200

    1.1. Входные данные в тесте [#400505]

--- test/ru/job4j/calculate/X2Test.java
+++ test/ru/job4j/calculate/X2Test.java
@@ -8,14 +8,14 @@ import static org.junit.Assert.*;
 public class X2Test {
 
     @Test
-    public void whenA1B1C0X1Then2() {
+    public void whenA1B1C1X0Then1() {
         // Входные параметры. Их будет 4 штуки.
         int a = 1;
         int b = 1;
-        int c = 0;
-        int x = 1;
+        int c = 1;
+        int x = 0;
         //Ожидаемое значение. это всегда одна переменная.
-        int expected = 2;
+        int expected = 1;
         // Вызов метода который хотим проверить.
         // Результат вычесления. Это всегда одна переменная.
 
