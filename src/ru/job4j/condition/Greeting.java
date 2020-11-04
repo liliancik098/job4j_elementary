45d5ad290dede8dac3d8fec01854380c2d846f93
pletosu.lilian <lilianzau@mail.ru>
2020-11-04 11:01:55 +0200

    1. Модульные тесты. [#400500]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,11 +2,11 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java!";
+        String idea = "I like Java! ";
         System.out.println(idea);
-        idea = "I like Java!" + "But I am a newbie.";
+        idea += "But I am a newbie. ";
         int year = 2020;
-        idea = "I like Java! " + "But I am a newbie. " + year;
+        idea += year;
         System.out.println(idea);
 
     }
