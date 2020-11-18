0b1c9c2dc6d36ebd9f03c0642f5903d15b32e993
pletosu.lilian <lilianzau@mail.ru>
2020-11-18 10:24:52 +0200

    6.0.2. Заполнение массива. [#400448]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,11 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4]
+                names[0] = "Плетосу Лилиан";
+                names[1] = "Хаширама Сэнджу";
+                names[2] = "Тобирама";
+                names[3] = "Пэйн";
 
          System.out.println(ages.length);
         System.out.println(surname.length);
